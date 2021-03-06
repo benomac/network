e1a3dd67adeb28b5f32931a2e8b4306f5f76b8f4
benomac <64007619+benomac@users.noreply.github.com>
2021-03-06 19:40:52 +0000

    Sort of got the viewing of all posts done :-O'''

--- network/static/network/net.js
+++ network/static/network/net.js
@@ -1,25 +1,64 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#allposts').addEventListener('click', load_all_posts());
-});
+document.addEventListener('DOMContentLoaded', function() {
+    if(document.querySelector('#newpost') !== null) {
+    document.querySelector('#newpost').addEventListener('click', function() {
+        new_post_view()
+    });
+    }
+    document.querySelector('#allposts').addEventListener('click', function() {
+        console.log("out")
+        window.location.href = '/';
+        all_posts_view()
+        load_all_posts();
+    });
+    
+    
+    load_all_posts();
+  });
+
+
 
 function load_all_posts() {
+    console.log("what")
+    
+    
     fetch('/all_posts')
     .then(response => response.json())
     .then(posts => {
        
         posts.forEach(post => {
-            console.log(post);
-            
+            let div1 = document.createElement('div')
+            div1.className = "posted";
             for (const [key, value] of Object.entries(post)) {
                 if (key !== "id") {
               
                     div = document.createElement('div');
                     div.className = key;
                     div.innerHTML = value;
-                    document.querySelector('#all-posts').append(div)
+                    div1.append(div)
                     
-                    }
-              }
-        });
+                }
+            }
+            console.log("when")
+            document.querySelector('#all-posts').append(div1);
+        })
+        
     });
+    if(document.querySelector('#newpost') !== null) {
+        document.querySelector('#new-post').style.display = 'none';
+    }
+    document.querySelector('#all-posts').style.display = 'block';
+}
+
+function new_post_view() {
+    console.log("dfgbdfnew")
+    document.querySelector('#new-post').style.display = 'block';
+    document.querySelector('#all-posts').style.display = 'none';
+}
+
+function all_posts_view() {
+    console.log("new")
+    if(document.querySelector('#newpost') !== null) {
+        document.querySelector('#new-post').style.display = 'none';
+    }
+    document.querySelector('#all-posts').style.display = 'block';
 }
